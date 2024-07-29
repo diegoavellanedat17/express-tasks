7a41ba45255c92d56c537ab9ffd6013bbe25fe9e
DiegoAvellaneda <48260188+diegoavellanedat17@users.noreply.github.com>
2024-07-28 22:20:01 -0500

    apply cors all origin temporary

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import taskRoutes from './routes/taskRoutes';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api', taskRoutes);
