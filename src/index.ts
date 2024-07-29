58ba43151a286d95b618bded0a29cfefab757a9c
DiegoAvellaneda <48260188+diegoavellanedat17@users.noreply.github.com>
2024-07-29 06:21:48 -0500

    fix: simplify the cors for the request

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: '*',
-    methods: 'GET,POST,PUT,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type,Authorization',
-  }),
-);
+app.use(cors());
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
